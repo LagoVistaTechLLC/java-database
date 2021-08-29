124247d0ee228abaed9af8fdf8e2a1e1f43db52e
Shawn Zernik <25804316+shawnzernik@users.noreply.github.com>
2021-08-29 00:19:06 -0500

    Record not found exception added. Maven repo added.

---
+++ src/main/java/com/lagovistatech/database/RecordNotFoundException.java
@@ -0,0 +1,6 @@
+package com.lagovistatech.database;
+
+public class RecordNotFoundException extends Exception {
+	private static final long serialVersionUID = 1L;
+	public RecordNotFoundException(String message) { super(message); }
+}
